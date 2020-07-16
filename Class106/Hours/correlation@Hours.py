ba4ef57c2fd3ee1bd16104a83be17c4e323863d0
PranavSomayajiSastry <58330938+PranavSomayajiSastry@users.noreply.github.com>
2020-07-16 18:48:07 +0530

    Add files via upload

---
+++ Class106/Hours/correlation@Hours.py
@@ -0,0 +1,19 @@
+import csv
+import numpy as np
+def getDataSource(data_path):
+    IceCream=[]
+    temperature=[]
+    with open (data_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            IceCream.append(float(row["Ice-cream"]))
+            temperature.append(float(row["Temperature"]))
+    return{"x":temperature, "y":IceCream}
+def findCorrelation(dataSource):
+    correlation = np.corrcoef(dataSource["x"],dataSource["y"])
+    print("The Correlation Coefficient between the Data: \n",correlation[0,1])
+def Setup():
+    data_path = "hours.csv"
+    dataSource = getDataSource(data_path)
+    findCorrelation(dataSource)
+Setup()
